82363631fe545740b2fb1fabb55fb3f2aaa3fb30
DESKTOP-NLQA4UA\doool <questknight26@gmail.com>
2021-12-14 23:49:32 +0900

    #1

--- app/src/main/java/com/doool/gooey/MainActivity.kt
+++ app/src/main/java/com/doool/gooey/MainActivity.kt
@@ -2,10 +2,15 @@ package com.doool.gooey
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContent {
+            Test()
+        }
     }
 }
+
